c5aa3db6286aefa045a5268ab628922858cb616b
chihaamin <amin.chiha@gmail.com>
2025-08-10 23:37:23 +0100

    Refactor binary_tree_height function for improved readability and logic flow

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,18 +11,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-	size_t height;
+	int l_height, r_height;
 
-	if (!tree)
+	if (
+		tree == NULL ||
+		(tree->left == NULL && tree->right == NULL && tree->parent == NULL))
 		return (0);
-
-	/* Recursively find the height of the left subtree */
-	left_height = binary_tree_height(tree->left);
-	/* Recursively find the height of the right subtree */
-	right_height = binary_tree_height(tree->right);
-
-	/* Return the greater height plus one for the current node */
-	height = left_height > right_height ? left_height : right_height;
-	return (height + 1);
+	else if (tree->parent != NULL)
+	{
+		l_height = binary_tree_height(tree->left);
+		r_height = binary_tree_height(tree->right);
+		return (1 + (l_height > r_height ? l_height : r_height));
+	}
+	return (binary_tree_height(tree->left));
+	return (binary_tree_height(tree->right));
 }
