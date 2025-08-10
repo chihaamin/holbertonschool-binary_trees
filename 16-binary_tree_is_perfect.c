8f58ea645ecef5d1864168d820b8fb70f5123bea
chihaamin <amin.chiha@gmail.com>
2025-08-10 23:46:49 +0100

    Refactor binary_tree_is_perfect function and implement binary_tree_size, binary_tree_height, and _pow functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,25 +1,74 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
- * Return: 1 if tree is perfect, 0 otherwise
+ *binary_tree_size - function measures the size of a binary tree
+ *@tree: pointer to the root node of the tree to measure the size
+ *Return: size of tree
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	size_t left_height, right_height, leaves_count, nodes_count;
 
+size_t binary_tree_size(const binary_tree_t *tree)
+{
 	if (tree == NULL)
 		return (0);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+/**
+ *binary_tree_height - function measures the height of binary tree
+ *@tree: pointer to the root node of the tree to measure the height
+ *Return: value to return
+ */
 
-	if (left_height != right_height)
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int l_height, r_height;
+
+	if (
+		tree == NULL ||
+		(tree->left == NULL && tree->right == NULL && tree->parent == NULL))
 		return (0);
+	else if (tree->parent != NULL)
+	{
+		l_height = binary_tree_height(tree->left);
+		r_height = binary_tree_height(tree->right);
+		return (1 + (l_height > r_height ? l_height : r_height));
+	}
+	return (binary_tree_height(tree->left));
+	return (binary_tree_height(tree->right));
+}
 
-	leaves_count = binary_tree_leaves(tree);
-	nodes_count = binary_tree_nodes(tree) + 1;
+/**
+ *_pow - function calculates base to the power of exponent
+ *@base: base to insert
+ *@exponent: exponent to insert
+ *Return: result
+ */
 
-	return (leaves_count == (1 << left_height) &&
-			nodes_count == (2 * leaves_count - 1));
+size_t _pow(int base, int exponent)
+{
+	int result = 1;
+	int i;
+
+	if (exponent < 0)
+	{
+		base = 1 / base;
+		exponent = -exponent;
+	}
+
+	for (i = 0; i < exponent; i++)
+		result *= base;
+	result *= base;
+	return (result);
+}
+
+/**
+ *binary_tree_is_perfect - function checks if a binary is perfect
+ *@tree: pointer to the root node of the tree to check
+ *Return: value to return
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (_pow(2, binary_tree_height(tree)) - 1 == binary_tree_size(tree))
+		return (1);
+	return (0);
 }
